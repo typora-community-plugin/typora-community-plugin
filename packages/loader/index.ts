1c6a21a0e8f7d14f0196cf15f70a1436a74f576e
plylrnsdy <plylrnsdy@163.com>
2023-10-26 01:02:18 +0800

    refactor(loader): compatible with macOS

--- packages/loader/index.ts
+++ packages/loader/index.ts
@@ -1,21 +1,36 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import { _options } from 'typora'
+import { File, _options, bridge, reqnode } from 'typora'
 
-const mountFolder = _options.mountFolder
-  ?? path.dirname(_options.initFilePath)
 
-const envPath = `${mountFolder}/.typora/env.json`
-const settings = fs.existsSync(envPath)
-  ? fs.readFileSync(envPath, 'utf8')
-  : '{}'
-const env = JSON.parse(settings)
+(function main() {
 
-const loaderConfigPath = `${_options.userDataPath}/plugins/loader.json`
-const loaderConfig = JSON.parse(fs.readFileSync(loaderConfigPath, 'utf8'))
+  const mountFolder = _options.mountFolder
+    ?? dirname(_options.initFilePath)
 
-const pluginCore = env.PLUGIN_CORE_PATH
-  ? path.resolve(env.PLUGIN_CORE_PATH.replace(/\{VAULT\}/, mountFolder))
-  : `./${loaderConfig.coreVersion}/core.js`
+  const envPath = `${mountFolder}/.typora/env.json`
+  const settings = readText(envPath) || '{}'
+  const env = JSON.parse(settings)
 
-import(pluginCore)
+  const loaderConfigPath = `${_options.userDataPath}/plugins/loader.json`
+  const loaderConfig = JSON.parse(readText(loaderConfigPath))
+
+  const pluginCore = env.PLUGIN_CORE_PATH
+    ? env.PLUGIN_CORE_PATH.replace(/\{VAULT\}/, mountFolder)
+    : `./${loaderConfig.coreVersion}/core.js`
+
+  import(pluginCore)
+
+})()
+
+function dirname(filepath: string) {
+  return filepath.split(/\b(?=[\\\/]+)/).slice(0, -1).join('')
+}
+
+function readText(filepath: string) {
+  if (File.isNode) {
+    const fs = reqnode('fs')
+    return fs.existsSync(filepath) ? fs.readFileSync(filepath, 'utf8') : ''
+  }
+  else {
+    return bridge.callSync('path.readText', filepath)
+  }
+}
