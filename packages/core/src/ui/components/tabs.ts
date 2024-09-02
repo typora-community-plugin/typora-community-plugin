b5c3649bf538fe2f728a8b284d4513059af0d8a1
plylrnsdy <plylrnsdy@163.com>
2024-09-03 00:44:29 +0800

    refactor(core/ui/component/tabs): extract tabs component

---
+++ packages/core/src/ui/components/tabs.ts
@@ -0,0 +1,137 @@
+import './tabs.scss'
+import { html } from "src/utils"
+import { View } from "src/ui/common/view"
+import { draggable } from "./draggable"
+
+
+interface TabContainerProps {
+  className?: string
+  onToggle: (tabId: string) => void
+  onClose: (tabId: string) => void
+}
+
+export class TabContainer extends View {
+
+  container: HTMLElement
+
+  constructor(private props: TabContainerProps) {
+    super()
+
+    this.containerEl = html`<div class="typ-tabs-wrapper ${props.className}"></div>`
+    this.containerEl.append(this.container =
+      $('<div class="typ-tabs"></div>')
+        // handle: left click
+        .on('click', event => {
+          const $clickedEl = $(event.target)
+          const $tab = $clickedEl.closest('.typ-tab')
+          if ($clickedEl.hasClass('typ-close')) {
+            this.closeTab($tab.get(0))
+          }
+          else {
+            if ($tab.hasClass('active')) return
+            this.toggleTab($tab.get(0))
+          }
+        })
+        // handle: middle click
+        .on('mousedown', event => {
+          if (event.button !== 1) return
+
+          const tab = event.target as HTMLElement
+          this.closeTab(tab)
+        })
+        // handle: scroll
+        .on('wheel', (event) => {
+          event.preventDefault()
+          const el = event.target as HTMLElement
+          let tabs
+          if (tabs = el.closest('.typ-tabs-wrapper')) {
+            const evt = event.originalEvent as WheelEvent
+            tabs.scrollLeft += evt.deltaY
+          }
+        })
+        .get(0)
+    )
+
+    // handle: draggable
+    draggable(this.containerEl, 'x')
+  }
+
+  showTab(tabEl: HTMLElement) {
+    tabEl.parentElement!.parentElement!.scrollLeft = tabEl.offsetLeft
+  }
+
+  addTab(tab: Tab) {
+    this.activeTab(tab.containerEl)
+    this.container.append(tab.containerEl)
+    this.showTab(tab.containerEl)
+  }
+
+  renameTab(tabEl: HTMLElement, tab: Tab) {
+    tabEl.replaceWith(tab.containerEl)
+  }
+
+  hideTabExtension(isHide: boolean) {
+    $('.typ-tabs-wrapper').toggleClass('typ-hide-ext', isHide)
+  }
+
+  activeTab(tabEl: HTMLElement) {
+    this.container.querySelectorAll('.typ-tab')
+      .forEach(el => el.classList.remove('active'))
+
+    tabEl.classList.add('active')
+  }
+
+  toggleTab(tabEl: HTMLElement) {
+    const tabId = tabEl.dataset.id!
+
+    this.container.querySelectorAll('.typ-tab')
+      .forEach(el => el.classList.remove('active'))
+
+    tabEl.classList.add('active')
+
+    this.props.onToggle(tabId)
+  }
+
+  closeTab(tabEl: HTMLElement) {
+    if (tabEl.classList.contains('active')) {
+      const siblingTab = this.getSiblingTab(tabEl)
+      this.toggleTab(siblingTab)
+    }
+
+    const tabId = tabEl.dataset.id!
+    this.props.onClose(tabId)
+  }
+
+  closeRightTabs(tabEl: HTMLElement) {
+    const tabEls = Array.from(this.container.children) as HTMLElement[]
+    const currentIdx = tabEls.findIndex(el => el.dataset.id === tabEl.dataset.id!)
+    const rightTabEls = tabEls.slice(currentIdx).slice(1)
+    rightTabEls.forEach(el => this.closeTab(el))
+  }
+
+  getSiblingTab(tabEl: HTMLElement) {
+    return (tabEl.previousElementSibling
+      ?? tabEl.nextElementSibling) as HTMLElement
+  }
+}
+
+interface TabProps {
+  id: string
+  text: string | (() => HTMLElement)
+  title?: string
+}
+
+export class Tab extends View {
+  constructor(props: TabProps) {
+    super()
+
+    this.containerEl = $(`<div class="typ-tab" data-id="${props.id}" draggable="true"></div>`)
+      .attr('title', props.title || '')
+      .append(typeof props.text === 'function'
+        ? props.text()
+        : props.text
+      )
+      .append(html`<i class="typ-icon typ-close"></i>`)
+      .get(0)
+  }
+}
--- packages/core/src/ui/tabs/tabs-view.ts
+++ packages/core/src/ui/tabs/tabs-view.ts
@@ -2,13 +2,12 @@ import './tabs-view.scss'
 import path from 'src/path'
 import { editor } from "typora"
 import decorate from "@plylrnsdy/decorate.js"
+import { Component } from 'src/common/component'
 import { useService } from 'src/common/service'
 import { useEventBus } from 'src/common/eventbus'
-import { draggable } from 'src/ui/components/draggable'
 import { Menu } from 'src/ui/components/menu'
+import { Tab, TabContainer } from 'src/ui/components/tabs'
 import { html, truncate } from 'src/utils'
-import { Component } from 'src/common/component'
-import { View } from '../common/view'
 
 
 const MAX_LENGHT = { length: 20, omission: '…' }
@@ -43,6 +42,7 @@ export class TabsView extends Component {
 
   onload() {
     this.container ??= new TabContainer({
+      className: 'typ-file-tabs',
       onToggle: (tabId) => this.toggleTab(tabId),
       onClose: (tabId) => this.removeTab(tabId),
     })
@@ -52,7 +52,7 @@ export class TabsView extends Component {
     const editorContainer = document.querySelector('content')!
     editorContainer.append(this.container.containerEl)
 
-    document.body.classList.add('typ-tabs--enable')
+    document.body.classList.add('typ-file-tabs--enable')
 
     this.register(
       this.workspace.on('file:will-open', () => {
@@ -102,7 +102,7 @@ export class TabsView extends Component {
   }
 
   onunload() {
-    document.body.classList.remove('typ-tabs--enable')
+    document.body.classList.remove('typ-file-tabs--enable')
     this.container.containerEl.remove()
   }
 
@@ -182,137 +182,6 @@ export class TabsView extends Component {
   removeRight(path: string) {
     editor.library.openFile(path)
     this.container.closeRightTabs(this.tabs.get(path)!)
-  }
-}
-
-interface TabContainerProps {
-  onToggle: (tabId: string) => void
-  onClose: (tabId: string) => void
-}
-
-class TabContainer extends View {
-
-  container: HTMLElement
-
-  constructor(private props: TabContainerProps) {
-    super()
-
-    this.containerEl = html`<div class="typ-tabs-wrapper"></div>`
-    this.containerEl.append(this.container =
-      $('<div class="typ-tabs"></div>')
-        // handle: left click
-        .on('click', event => {
-          const $clickedEl = $(event.target)
-          const $tab = $clickedEl.closest('.typ-tab')
-          if ($clickedEl.hasClass('typ-close')) {
-            this.closeTab($tab.get(0))
-          }
-          else {
-            if ($tab.hasClass('active')) return
-            this.toggleTab($tab.get(0))
-          }
-        })
-        // handle: middle click
-        .on('mousedown', event => {
-          if (event.button !== 1) return
-
-          const tab = event.target as HTMLElement
-          this.closeTab(tab)
-        })
-        // handle: scroll
-        .on('wheel', (event) => {
-          event.preventDefault()
-          const el = event.target as HTMLElement
-          let tabs
-          if (tabs = el.closest('.typ-tabs-wrapper')) {
-            const evt = event.originalEvent as WheelEvent
-            tabs.scrollLeft += evt.deltaY
-          }
-        })
-        .get(0)
-    )
-
-    // handle: draggable
-    draggable(this.containerEl, 'x')
-  }
-
-  showTab(tabEl: HTMLElement) {
-    tabEl.parentElement!.parentElement!.scrollLeft = tabEl.offsetLeft
-  }
-
-  addTab(tab: Tab) {
-    this.activeTab(tab.containerEl)
-    this.container.append(tab.containerEl)
-    this.showTab(tab.containerEl)
-  }
-
-  renameTab(tabEl: HTMLElement, tab: Tab) {
-    tabEl.replaceWith(tab.containerEl)
-  }
-
-  hideTabExtension(isHide: boolean) {
-    $('.typ-tabs-wrapper').toggleClass('typ-hide-ext', isHide)
-  }
-
-  activeTab(tabEl: HTMLElement) {
-    this.container.querySelectorAll('.typ-tab')
-      .forEach(el => el.classList.remove('active'))
-
-    tabEl.classList.add('active')
-  }
-
-  toggleTab(tabEl: HTMLElement) {
-    const tabId = tabEl.dataset.id!
-
-    this.container.querySelectorAll('.typ-tab')
-      .forEach(el => el.classList.remove('active'))
-
-    tabEl.classList.add('active')
-
-    this.props.onToggle(tabId)
-  }
-
-  closeTab(tabEl: HTMLElement) {
-    if (tabEl.classList.contains('active')) {
-      const siblingTab = this.getSiblingTab(tabEl)
-      this.toggleTab(siblingTab)
-    }
-
-    const tabId = tabEl.dataset.id!
-    this.props.onClose(tabId)
-  }
-
-  closeRightTabs(tabEl: HTMLElement) {
-    const tabEls = Array.from(this.container.children) as HTMLElement[]
-    const currentIdx = tabEls.findIndex(el => el.dataset.id === tabEl.dataset.id!)
-    const rightTabEls = tabEls.slice(currentIdx).slice(1)
-    rightTabEls.forEach(el => this.closeTab(el))
-  }
-
-  getSiblingTab(tabEl: HTMLElement) {
-    return (tabEl.previousElementSibling
-      ?? tabEl.nextElementSibling) as HTMLElement
-  }
-}
-
-interface TabProps {
-  id: string
-  text: string | (() => HTMLElement)
-  title?: string
-}
-
-class Tab extends View {
-  constructor(props: TabProps) {
-    super()
-
-    this.containerEl = $(`<div class="typ-tab" data-id="${props.id}" draggable="true"></div>`)
-      .attr('title', props.title || '')
-      .append(typeof props.text === 'function'
-        ? props.text()
-        : props.text
-      )
-      .append(html`<i class="typ-icon typ-close"></i>`)
-      .get(0)
   }
 }
 
