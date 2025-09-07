f28eb2f2dc59d55f3318d980b579db41feb4f391
plylrnsdy <plylrnsdy@163.com>
2025-09-07 18:57:01 +0800

    refactor(core): compatible `String.trimEnd()` with macOS

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -1,6 +1,7 @@
 import 'core-js/modules/es.array.at'
 import 'core-js/modules/es.array.flat-map'
 import 'core-js/modules/es.global-this'
+import 'core-js/modules/es.string.trim-end'
 import 'src/setup'
 import { useService } from 'src/common/service'
 
