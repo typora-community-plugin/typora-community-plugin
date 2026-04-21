a1f101fb76a33494128e7a0020efdee290a65ad7
plylrnsdy <plylrnsdy@163.com>
2026-04-22 01:34:39 +0800

    feat(core/ui/components/notice): support `options.type` and add a new command `clearAll`

--- packages/core/src/ui/components/notice.ts
+++ packages/core/src/ui/components/notice.ts
@@ -1,20 +1,52 @@
 import './notice.scss'
 import { Component } from 'src/common/component'
+import { useService } from 'src/common/service'
 import { Closeable, View } from 'src/ui/common/view'
 import { html } from 'src/utils'
 
 
+/** @private */
 class NoticeContainer extends Component implements Closeable {
 
   containerEl: HTMLElement
+  notices: Notice[] = []
 
+  constructor() {
+    super()
+
+    setTimeout(() => {
+      const commands = useService('command-manager')
+      const { t } = useService('i18n')
+      this.register(
+        commands.register({
+          id: 'core.notice:clear-all',
+          title: t.notice.clearAll,
+          scope: 'global',
+          callback: () => this.clearAll(),
+        }))
+    }, 167)
+  }
+
+  /** @private */
   onload() {
     this.containerEl = html`<div class="typ-notice__container" style="display: none;"></div>`
     document.body.append(this.containerEl)
   }
 
+  /** @private */
   onunload() {
     this.containerEl.remove()
+  }
+
+  add(notice: Notice) {
+    this.notices.push(notice);
+  }
+  remove(notice: Notice) {
+    this.notices = this.notices.filter(item => item !== notice);
+  }
+
+  clearAll() {
+    [...this.notices].forEach(notice => notice.close());
   }
 
   open() {
@@ -29,22 +61,34 @@ class NoticeContainer extends Component implements Closeable {
 
 export const noticeContainer = new NoticeContainer()
 
+
+interface NoticeOptions {
+  type?: 'success' | 'warning' | 'error'
+  duration?: number
+}
+
 export class Notice extends View {
 
   /**
-   * @param delay Hide notice after `delay` ms. `delay = 0` will not be hidden.
+   * @param duration Hide notice after `duration` ms. `duration = 0` will not be hidden.
    */
-  constructor(message: string, delay = 5000) {
+  constructor(message: string, duration?: number)
+  constructor(message: string, options?: NoticeOptions)
+  constructor(message: string, options?: number | NoticeOptions) {
     super()
-    this.containerEl = $(`<div class="typ-notice"></div>`)
-      .append(`<div>${message}</div>`)
+
+    const duration = (typeof options === 'number' ? options : options.duration) ?? 5000
+    const type = (<any>options).type ?? 'info'
+
+    this.containerEl = $(`<div class="typ-notice ${type}"></div>`)
+      .append(`<div class="typ-notice__content">${message}</div>`)
       .append($('<div class="typ-notice__close"><i class="typ-icon typ-close"></i></div>')
         .on('click', () => this.close()))
       .get(0)
 
     this.show();
 
-    (delay > 0) && setTimeout(() => this.close(), delay)
+    (duration > 0) && setTimeout(() => this.close(), duration)
   }
 
   /**
@@ -68,11 +112,15 @@ export class Notice extends View {
 
   show() {
     noticeContainer.containerEl.append(this.containerEl)
+    noticeContainer.add(this)
     noticeContainer.open()
+    requestAnimationFrame(() => this.containerEl.classList.add('show'))
   }
 
   close() {
     this.containerEl.remove()
+    noticeContainer.remove(this)
     noticeContainer.close()
   }
 }
+
