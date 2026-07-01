1608784d5e181f0e5f68d641040f8003d8d28578
plylrnsdy <plylrnsdy@163.com>
2026-07-01 15:14:26 +0800

    refactor(core/settings): migrate legacy workspace settings in v1→v2 migration

--- packages/core/src/settings/settings-migration.ts
+++ packages/core/src/settings/settings-migration.ts
@@ -1,14 +1,22 @@
+import { AppSettings } from "src/app"
 import { SettingMigrations } from "./settings"
+import { KEY_OF_ENABLED_PLUGINS } from "src/plugin-internal/internal-plugin-manager"
+import { PLUGIN_WORKSPACE_ID } from "src/plugin-internal/plugins/plugin-workspace"
 
 
 export function createSettingsMigration(): [number, SettingMigrations] {
   let currentVersion = 0
   const migrations = new SettingMigrations()
 
-  migrations.addMigration(1, currentVersion = 2, (oldStores) => {
-    const { settings } = oldStores
+  migrations.addMigration(1, currentVersion = 2, ({ settings }: { settings: AppSettings }) => {
+    // @ts-ignore
+    settings[KEY_OF_ENABLED_PLUGINS][PLUGIN_WORKSPACE_ID] = settings.useWorkspace
+
+    // @ts-ignore
     delete settings.showFileTabs
+    // @ts-ignore
     delete settings.useWorkspace
+
     return {
       version: 2,
       settings,
