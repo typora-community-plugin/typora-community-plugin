6ebef5f78c16abe80dc6754a713ea64cd59e7628
plylrnsdy <plylrnsdy@163.com>
2026-07-05 02:00:40 +0800

    fix(core/settings-migration): guard against undefined enabled plugins during first migration

--- packages/core/src/settings/settings-migration.ts
+++ packages/core/src/settings/settings-migration.ts
@@ -10,12 +10,15 @@ export function createSettingsMigration(): [number, SettingMigrations] {
 
   migrations.addMigration(1, currentVersion = 2, ({ settings }: { settings: AppSettings }) => {
     // @ts-ignore
-    settings[KEY_OF_ENABLED_PLUGINS][PLUGIN_WORKSPACE_ID] = settings.useWorkspace
-
-    // @ts-ignore
     delete settings.showFileTabs
-    // @ts-ignore
-    delete settings.useWorkspace
+
+    if ('useWorkspace' in settings) {
+      if (!settings[KEY_OF_ENABLED_PLUGINS]) settings[KEY_OF_ENABLED_PLUGINS] = {}
+      // @ts-ignore
+      settings[KEY_OF_ENABLED_PLUGINS][PLUGIN_WORKSPACE_ID] = settings.useWorkspace
+      // @ts-ignore
+      delete settings.useWorkspace
+    }
 
     return {
       version: 2,
